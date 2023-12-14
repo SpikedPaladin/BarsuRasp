d8abcb689d2771e3f7759786e3d823f6f8f8c60e
PaladinDev <paladingames1@gmail.com>
2023-12-14 18:31:16 +0300

    Use proper icon for bus direction switch

--- app/src/main/kotlin/me/paladin/barsurasp/ui/components/bus/BusToolbarRow.kt
+++ app/src/main/kotlin/me/paladin/barsurasp/ui/components/bus/BusToolbarRow.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.outlined.LocationOn
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -14,7 +13,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import me.paladin.barsurasp.R
 import me.paladin.barsurasp.ui.components.RoundedBox
 
 @Composable
@@ -37,7 +38,7 @@ fun BusToolbarRow(
                 IconButton(
                     onClick = changeDirection
                 ) {
-                    Icon(imageVector = Icons.Default.ArrowDropDown, contentDescription = null)
+                    Icon(painterResource(R.drawable.ic_direction_switch), contentDescription = null)
                 }
             }
         }
