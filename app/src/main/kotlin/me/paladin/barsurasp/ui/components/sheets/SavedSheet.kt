1632e2ffb3f63d67347d324f762115e53f979a5a
PaladinDev <paladingames1@gmail.com>
2023-12-17 21:17:26 +0300

    Add dividers to SavedSheet

--- app/src/main/kotlin/me/paladin/barsurasp/ui/components/sheets/SavedSheet.kt
+++ app/src/main/kotlin/me/paladin/barsurasp/ui/components/sheets/SavedSheet.kt
@@ -10,10 +10,11 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Button
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -76,23 +77,24 @@ private fun SavedSheetContent(
     openFaculties: () -> Unit,
 ) {
     LazyColumn(
-        Modifier.padding(bottom = 24.dp, start = 8.dp, end = 8.dp)
+        Modifier.padding(bottom = 16.dp, start = 8.dp, end = 8.dp)
     ) {
         item {
             Text(
                 text = "Сохраненные",
                 style = MaterialTheme.typography.labelLarge,
-                color = MaterialTheme.colorScheme.primary
+                color = MaterialTheme.colorScheme.primary,
+                modifier = Modifier
+                    .padding(4.dp)
             )
         }
 
-        items(savedItems.toList()) { item ->
-            val parts = item.split(":", limit = 2)
-            val title = parts[0]
-            val subtitle = if (parts.size > 1)
-                if (parts[1] == "") null
-                else parts[1]
-            else null
+        val selectedIndex = if (mainGroup != null)
+            savedItems.indexOfFirst { it.startsWith(mainGroup) }
+        else -1
+
+        itemsIndexed(savedItems.toList()) { index, item ->
+            val (title, subtitle) = item.splitItem()
 
             Spacer(Modifier.size(2.dp))
             SavedItem(
@@ -103,6 +105,8 @@ private fun SavedSheetContent(
                 onClick = { groupSelected(title) }
             )
             Spacer(Modifier.size(2.dp))
+            if (selectedIndex != index && selectedIndex - 1 != index && index != savedItems.size - 1)
+                HorizontalDivider(Modifier.padding(start = 4.dp, end = 4.dp))
         }
         item {
             Box(
@@ -167,6 +171,17 @@ private fun SavedItem(
             )
         }
     }
+}
+
+private fun String.splitItem(): Pair<String, String?> {
+    val parts = split(":", limit = 2)
+    val title = parts[0]
+    val subtitle = if (parts.size > 1)
+        if (parts[1] == "") null
+        else parts[1]
+    else null
+
+    return Pair(title, subtitle)
 }
 
 @Preview
